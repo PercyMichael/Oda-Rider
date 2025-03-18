171040f534944a6af94c006de9d48e6366146c48
Percy Michael <consultdizayndomains@gmail.com>
2025-03-18 17:54:05 +0300

    hi

--- lib/screen/auth/login.dart
+++ lib/screen/auth/login.dart
@@ -92,6 +92,8 @@ class _LoginState extends State<Login> {
                   Get.toNamed('/otp');
                 },
               ),
+
+              ///
               const Row(
                 children: [
                   Expanded(
